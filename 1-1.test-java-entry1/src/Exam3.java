ec6a785b477205106a7eb5456dd122978d13fe40
AKI <yusuke.aki@rakus-partners.co.jp>
2026-01-15 10:07:55 +0900

    プログラミング入門１

---
+++ 1-1.test-java-entry1/src/Exam3.java
@@ -0,0 +1,11 @@
+public class Exam3 {
+    public static void main(String[] args) {
+        int product1 =200;
+        int product2 =250;
+        double tax =0.1;
+        System.out.println(product1*3+product2*4+"円");
+        System.out.println((int)((product1*3+product2*4)*tax)+"円");
+        System.out.println((int)((product1*3+product2*4)*1.1)+"円");
+    }
+
+}
---
+++ 1-1.test-java-entry1/src/Exam5.java
@@ -0,0 +1,14 @@
+public class Exam5 {
+    public static void main(String[] args) {
+        System.out.println("int");
+        System.out.println("String");
+        System.out.println("double");
+        System.out.println("short");
+        System.out.println("byte");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+    }
+
+}
