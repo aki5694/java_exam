51ed582c4f65eaf32a8ac8dc07cc3f6adde2b26d
AKI <yusuke.aki@rakus-partners.co.jp>
2026-01-20 09:51:44 +0900

    オブジェクト指向入門

---
+++ 2.test-object-oriented/src/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+
+    int energy = 50;
+
+    void sleep(){
+        energy = energy + 50;
+        System.out.println("エネルギーが" + energy + "になりました。");
+        if (energy<=100){
+            System.out.println("フルパワー！");
+        }
+    }
+
+    void study(){
+        energy = energy - 30;
+        System.out.println("勉強しました。エネルギーが" + energy + "になりました。");
+        if(energy<=0){
+            System.out.println("寝てください");
+        }
+    }
+
+}
---
+++ 2.test-object-oriented/src/StudentMain.java
@@ -0,0 +1,13 @@
+public class StudentMain {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.study();
+        student.study();
+        student.sleep();
+        student.sleep();
+
+    }
+
+
+}
